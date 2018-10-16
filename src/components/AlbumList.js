e4c2fc424549d8b752927a58020d0850f27fc80d
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-17 00:25:15 +0800

    Converted to class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = (props) => {
-    return (
-        <View>
-            <Text>Album List!!!!</Text>
-        </View>
-    );
-};
+class AlbumList extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Album Lister!!!!</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
