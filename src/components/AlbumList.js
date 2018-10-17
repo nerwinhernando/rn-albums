0aa940404aa426678e58a3e3bbb5f1e0d20b134d
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-17 15:44:07 +0800

    renderAlbums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -8,11 +8,16 @@ class AlbumList extends React.Component {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
             .then(response => this.setState({albums: response.data }));
     }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
+
     render() {
         console.log(this.state);
         return (
             <View>
-                <Text>Album Lister!!!!</Text>
+                { this.renderAlbums() }
             </View>
         );
     }
