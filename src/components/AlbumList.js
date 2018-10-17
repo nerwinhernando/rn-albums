38592358816b49cc693b920db807d2dd7261b5a0
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-17 13:34:46 +0800

    you have to update npm, delete node_module, update react-native-cli, restart metro

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends React.Component {
+    componentDidMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response));
+    }
     render() {
         return (
             <View>
