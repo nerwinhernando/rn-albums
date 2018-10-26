3d8f120b5cba6b42b7cdd2bf07255ab5f8bac341
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-26 13:27:04 +0800

    Dividing Cards into sections

---
+++ src/components/CardSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+const CardSection = (props) => {
+    return(
+        <View style={styles.containerStyle} >
+            {props.children}
+        </View>
+    );
+};
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        padding: 5
+    }
+};
+
+export default CardSection;
