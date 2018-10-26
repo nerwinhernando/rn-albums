624edbd94d02932050e31bbcc1c6fcd368469a60
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-26 19:05:05 +0800

    Add Button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
+import Button from './Button';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -32,6 +33,9 @@ const AlbumDetail = ({ album }) => {
                     style={imageStyle}
                     source={{ uri: image }} 
                 />
+            </CardSection>
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
